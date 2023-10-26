b262861e6dc9ba96a3299e9b875ab567da0eb380
Daiki Anjiki <daikianjiki@gmail.com>
2023-10-26 10:31:34 -0700

    completed a quick Todo List

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import TodoList from './todos/TodoList'
 
-const App =() => {
+const App = () => {
     return (
         <div className="App">
-            <h1>Hello, World!</h1>
+            <TodoList />
         </div>
     )
 }
